ad0c623839b405930235569118abf6495ab09b40
elalamielhassaniotmane <elalamielhassani@gmail.com>
2020-07-19 21:21:53 +0200

    fix

--- blog/src/main/java/com/blog/blog/Controllers/PostController.java
+++ blog/src/main/java/com/blog/blog/Controllers/PostController.java
@@ -1,12 +1,13 @@
 package com.blog.blog.Controllers;
 
+import com.blog.blog.data.entities.PostEntity;
 import com.blog.blog.services.PostService;
-import data.entities.PostEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
 import java.util.List;
 
 @RestController
@@ -16,12 +17,13 @@ public class PostController {
 
 
     @GetMapping("/posts")
-  public List<PostEntity> getAllPosts(){
-    return this.postservice.getAllPosts();
-}
-@PostMapping("/post")
+    public List<PostEntity> getAllPosts() {
+        return this.postservice.getAllPosts();
+    }
 
-public PostEntity addPost(@RequestBody PostEntity postEntity){
-return this.postservice.addPost(postEntity);
-}
+    @PostMapping("/post")
+
+    public PostEntity addPost(@RequestBody PostEntity postEntity) {
+        return this.postservice.addPost(postEntity);
+    }
 }
--- blog/src/main/java/com/blog/blog/services/PostService.java
+++ blog/src/main/java/com/blog/blog/services/PostService.java
@@ -1,33 +1,30 @@
 package com.blog.blog.services;
 
-import data.entities.PostEntity;
-import data.repository.PostRepository;
+import com.blog.blog.data.entities.PostEntity;
+import com.blog.blog.data.repository.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Example;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
-public class PostService  {
+public class PostService {
     @Autowired
     private PostRepository postreposetry;
-     public List<PostEntity> getAllPosts() {
 
-         return postreposetry.findAll();
+    public List<PostEntity> getAllPosts() {
+
+        return postreposetry.findAll();
     }
+
     public List<PostEntity> getPostByUserId(long userId) {
 
         return postreposetry.findPostsByUserId(userId);
     }
-    public PostEntity addPost(PostEntity postEntity){
+
+    public PostEntity addPost(PostEntity postEntity) {
         return this.postreposetry.save(postEntity);
     }
-
 
 
 }
