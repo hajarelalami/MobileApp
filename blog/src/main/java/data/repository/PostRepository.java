6738159b06c9c67b20ccfc306e41b601bc247b47
hajarelalami <58108269+hajarelalami@users.noreply.github.com>
2020-07-19 20:12:53 +0100

    post1

--- blog/src/main/java/data/repository/PostRepository.java
+++ blog/src/main/java/data/repository/PostRepository.java
@@ -3,10 +3,11 @@ package data.repository;
 import data.entities.PostEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-
+@Repository
 public interface PostRepository extends JpaRepository<PostEntity, Long> {
     @Query( value = "SELECT * FROM  post WHERE user_id = ?1", nativeQuery = true)
     List<PostEntity> findPostsByUserId(long userId);
